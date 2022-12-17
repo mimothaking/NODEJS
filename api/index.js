cad02168f6518cb483ffec660311d83a13df4643
Machmachi Mohamed <61361554+mimothaking@users.noreply.github.com>
2022-12-17 20:50:31 +0100

    Hotel CRUD API

--- api/index.js
+++ api/index.js
@@ -28,10 +28,24 @@ app.get("/", (req,res)=>{
     res.send("HOLA AMIGOS")
 } )
 
-app.use("/auth", routeAuth);
-app.use("/users", routeUsers);
-app.use("/hotels", routeHotels);
-app.use("/rooms", routeRooms);
+//middleWares
+app.use(express.json())
+
+app.use("/api/auth", routeAuth);
+app.use("/api/users", routeUsers);
+app.use("/api/hotels", routeHotels);
+app.use("/api/rooms", routeRooms);
+
+app.use((err,req,res,next)=>{
+    const errorStatus = err.status || 500
+    const errorMessage = err.message || "Something went wrong !"
+    return res.status(errorStatus).json({
+        success: false,
+        status: errorStatus,
+        message: errorMessage,
+        stack: err.stack
+    })
+})
 
 app.listen(8800, ()=>{
     connect();
