614c49bbd27f05133ed8b2bd3f3ced0baec3f743
Machmachi Mohamed <61361554+mimothaking@users.noreply.github.com>
2022-12-17 21:21:26 +0100

    Making controllers

---
+++ api/controllers/hotel.js
@@ -0,0 +1,60 @@
+export const createHotel = async (req,res,next) => {
+    const newHotel = new Hotel(req.body)
+
+    try {
+        const savedHotel = await newHotel.save()
+        res.status(200).json(savedHotel)
+
+    } catch (err) {
+        next(err)
+    }
+
+}
+export const updateHotel = async (req, res, next) => {
+    const newHotel = new Hotel(req.body)
+
+    try {
+        const savedHotel = await newHotel.save()
+        res.status(200).json(savedHotel)
+
+    } catch (err) {
+        next(err)
+    }
+
+}
+export const deleteHotel = async (req, res, next) => {
+    const newHotel = new Hotel(req.body)
+
+    try {
+        const savedHotel = await newHotel.save()
+        res.status(200).json(savedHotel)
+
+    } catch (err) {
+        next(err)
+    }
+
+}
+export const geHotel = async (req, res, next) => {
+    const newHotel = new Hotel(req.body)
+
+    try {
+        const savedHotel = await newHotel.save()
+        res.status(200).json(savedHotel)
+
+    } catch (err) {
+        next(err)
+    }
+
+}
+export const getAllHotel = async (req, res, next) => {
+    const newHotel = new Hotel(req.body)
+
+    try {
+        const savedHotel = await newHotel.save()
+        res.status(200).json(savedHotel)
+
+    } catch (err) {
+        next(err)
+    }
+
+}
--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -1,22 +1,12 @@
 import express from "express";
 import Hotel from "../models/Hotel.js";
+import { createHotel } from "../controllers/hotel.js";
 import { CreateError } from "../utils/error.js";
 
 const router = express.Router();
 
 //CREATE
-router.post("/", async (req,res) =>{
-    
-    const newHotel = new Hotel(req.body)
-
-    try{
-        const savedHotel = await newHotel.save()
-        res.status(200).json(savedHotel)
-
-    }catch(err){
-        res.status(500).json(err)
-    }
-})
+router.post("/", createHotel)
 //UPDATE
 router.put("/:id", async (req, res) => {
 
