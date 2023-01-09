c8dfff560796c6eaa3609805b453410c5818070a
Machmachi Mohamed <61361554+mimothaking@users.noreply.github.com>
2023-01-09 14:18:09 +0100

    the full website

--- api/index.js
+++ api/index.js
@@ -1,53 +1,51 @@
-import  express  from "express";
-import dotenv from 'dotenv'
+import express from "express";
+import dotenv from "dotenv";
 import mongoose from "mongoose";
-import routeAuth from './routes/auth.js'
-import routeUsers from './routes/users.js'
-import routeHotels from './routes/hotels.js'
-import routeRooms from './routes/rooms.js'
+import authRoute from "./routes/auth.js";
+import usersRoute from "./routes/users.js";
+import hotelsRoute from "./routes/hotels.js";
+import roomsRoute from "./routes/rooms.js";
+import cookieParser from "cookie-parser";
+import cors from "cors";
 
-const app = express()
+const app = express();
+dotenv.config();
 
-dotenv.config()
-
-const connect = async ()=>{
-try {
-     await mongoose.connect(process.env.MONGO);
-        } catch (error) {
-            throw error;
-        }
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO);
+    console.log("Connected to mongoDB.");
+  } catch (error) {
+    throw error;
+  }
 };
 
+mongoose.connection.on("disconnected", () => {
+  console.log("mongoDB disconnected!");
+});
 
+//middlewares
+app.use(cors())
+app.use(cookieParser())
+app.use(express.json());
 
-mongoose.connection.on("disconnected", ()=>{
-    console.log('mongo disco :c')
-})
+app.use("/api/auth", authRoute);
+app.use("/api/users", usersRoute);
+app.use("/api/hotels", hotelsRoute);
+app.use("/api/rooms", roomsRoute);
 
-app.get("/", (req,res)=>{
-    res.send("HOLA AMIGOS")
-} )
+app.use((err, req, res, next) => {
+  const errorStatus = err.status || 500;
+  const errorMessage = err.message || "Something went wrong!";
+  return res.status(errorStatus).json({
+    success: false,
+    status: errorStatus,
+    message: errorMessage,
+    stack: err.stack,
+  });
+});
 
-//middleWares
-app.use(express.json())
-
-app.use("/api/auth", routeAuth);
-app.use("/api/users", routeUsers);
-app.use("/api/hotels", routeHotels);
-app.use("/api/rooms", routeRooms);
-
-app.use((err,req,res,next)=>{
-    const errorStatus = err.status || 500
-    const errorMessage = err.message || "Something went wrong !"
-    return res.status(errorStatus).json({
-        success: false,
-        status: errorStatus,
-        message: errorMessage,
-        stack: err.stack
-    })
-})
-
-app.listen(8800, ()=>{
-    connect();
-    console.log('connected hh')
-})
+app.listen(8800, () => {
+  connect();
+  console.log("Connected to backend.");
+});
